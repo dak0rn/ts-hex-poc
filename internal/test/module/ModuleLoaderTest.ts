ea403debbb4a279158d47d719b882fc1e80cd506
Daniel Koch <k@kdb.gmbh>
2020-12-06 21:03:20 +0000

    Add test for error handling behavior

--- internal/test/module/ModuleLoaderTest.ts
+++ internal/test/module/ModuleLoaderTest.ts
@@ -1,6 +1,6 @@
 import test from 'ava';
 import sinon from 'sinon';
-import ModuleLoader from '@internal/module/ModuleLoader';
+import ModuleLoader, { ModuleLoadingFailedException } from '@internal/module/ModuleLoader';
 import ConfigurationAdapter from '@internal/configuration/ConfigurationAdapter';
 import SystemConfiguration from '@internal/configuration/SystemConfiguration';
 import ApplicationConfiguration from '@internal/configuration/ApplicationConfiguration';
@@ -61,4 +61,53 @@ test('ModuleLoader.load returns the provided modules', t => {
     t.is(modules[0], mod1);
     t.is(modules[1], mod2);
     t.is(modules[2], mod3);
+});
+
+test('ModuleLoader.load throws ModuleLoadingFailedException upon Errors thrown', t => {
+    t.plan(2);
+
+    class ModuleStub implements ApplicationModule {
+        getClass(): new (...args: any[]) => any {
+            throw new Error('Method not implemented.');
+        }
+    }
+
+    const sc = new SystemConfiguration({ moduleFolder: 'fake-path', modules: ['a'] }, new StubAdapter(''));
+    const ml = new ModuleLoader(sc);
+
+    const loader = sinon.stub();
+
+    loader.onCall(0).throws(new Error('banana'));
+    ml.loader = loader;
+
+    try {
+        ml.load();
+    } catch (err) {
+        t.true(err instanceof ModuleLoadingFailedException);
+        t.true((err as Error).message.includes('banana'));
+    }
+});
+
+test('ModuleLoader.load throws ModuleLoadingFailedException upon non-Errors thrown', t => {
+    t.plan(1);
+
+    class ModuleStub implements ApplicationModule {
+        getClass(): new (...args: any[]) => any {
+            throw new Error('Method not implemented.');
+        }
+    }
+
+    const sc = new SystemConfiguration({ moduleFolder: 'fake-path', modules: ['a'] }, new StubAdapter(''));
+    const ml = new ModuleLoader(sc);
+
+    const loader = sinon.stub();
+
+    loader.onCall(0).throws(42);
+    ml.loader = loader;
+
+    try {
+        ml.load();
+    } catch (err) {
+        t.true(err instanceof ModuleLoadingFailedException);
+    }
 });
