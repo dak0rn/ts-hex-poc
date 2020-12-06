37df93d384a0d603efab8f1dfc58ee62e00ac681
Daniel Koch <k@kdb.gmbh>
2020-12-06 15:14:21 +0000

    Add reflect-metadata to ava hooks for usage with decorators

--- ava.config.js
+++ ava.config.js
@@ -2,6 +2,6 @@ export default {
     files: ['**/test/**/*.ts', '!**/dist/**', '!**/node_modules/**'],
     failFast: true,
     verbose: false,
-    require: ['ts-node/register', 'tsconfig-paths/register'],
+    require: ['ts-node/register', 'tsconfig-paths/register', 'reflect-metadata'],
     extensions: ['ts']
 };
--- ava.coverage.config.js
+++ ava.coverage.config.js
@@ -2,6 +2,6 @@ export default {
     files: ['**/test/**/*.ts', '!**/dist/**', '!**/node_modules/**'],
     failFast: true,
     verbose: false,
-    require: ['ts-node/register', 'tsconfig-paths/register', 'source-map-support/register'],
+    require: ['ts-node/register', 'tsconfig-paths/register', 'source-map-support/register', 'reflect-metadata'],
     extensions: ['ts']
 };
