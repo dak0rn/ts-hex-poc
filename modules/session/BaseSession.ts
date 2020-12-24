247a225f033ffc5a9cc9899fa91d5384d15e18a6
Daniel Koch <k@kdb.gmbh>
2020-12-24 20:50:46 +0100

    Add a base session class

---
+++ modules/session/BaseSession.ts
@@ -0,0 +1,19 @@
+import Serializable from '@core/io/Serializable';
+
+/**
+ * Base class for all sessions
+ */
+export default abstract class BaseSession implements Serializable<string> {
+    public abstract deserialize(input: string): void;
+    public abstract serialize(): string;
+
+    /**
+     * Returns the surrogate key for the session
+     * The surrogate key represents the identity of the session and allows
+     * to uniquely identify it. It will be used in the {@link SessionBackend}
+     * to store and retrieve the session from the persistence layer.
+     *
+     * @return
+     */
+    public abstract get surrogateKey(): string;
+}
