024b5292544f1df12c26e186946147286da90655
Daniel Koch <k@kdb.gmbh>
2020-12-26 08:00:07 +0100

    Remove types directory

--- core/types/lib.d.ts
+++
@@ -1,6 +0,0 @@
-export class ThreadLocal {
-    public store: any;
-    constructor(store: any);
-    run(callback: (...args: any[]) => unknown): void;
-    public static getStore(): unknown;
-}
--- core/types/modules.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ApplicationModule {
-    getClass(): { new (...args: any[]): any };
-}
-
-export interface ApplicationModuleLauncher {
-    launch(): Promise<unknown>;
-}
