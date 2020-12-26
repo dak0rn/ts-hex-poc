4544f440724b8ff7b2405b74e2f6e03836563024
Daniel Koch <k@kdb.gmbh>
2020-12-26 09:02:00 +0100

    Add a registry for transaction managers

---
+++ core/io/datastores/TransactionManagerRegistry.ts
@@ -0,0 +1,135 @@
+import SystemConfiguration from '@core/configuration/SystemConfiguration';
+import ApplicationContext from '@core/ioc/ApplicationContext';
+import CoreObject from '@core/shared/CoreObject';
+import { inject, injectable } from 'tsyringe';
+import { TransactionManager } from './TransactionManager';
+
+/**
+ * A {@link TransactionManager} was registered under a name already provided
+ */
+export class DuplicateTransactionManagerError extends Error {
+    /**
+     * Creates a new {@link DuplicateTransactionManagerError} with the given name
+     *
+     * @param name The duplicate key
+     */
+    constructor(name: string) {
+        super(`Duplicate transaction manager registration for name "${name}"`);
+    }
+}
+
+/**
+ * A default {@link TransactionManager} was configured but does not exist at runtime
+ */
+export class DefaultTransactionManagerDoesNotExistError extends Error {
+    /**
+     * Creates a new {@link DefaultTransactionManagerDoesNotExistError}
+     *
+     * @param name Name of the default
+     */
+    constructor(name: string) {
+        super(`The default transaction manager "${name}" does not exist`);
+    }
+}
+
+/**
+ * A {@link TransactionManager} was referenced but does not exist
+ */
+export class TransactionManagerDoesNotExistError extends Error {
+    /**
+     * Creates a new {@link TransactionManagerDoesNotExistError}
+     *
+     * @param name Name of the default
+     */
+    constructor(name: string) {
+        super(`The transaction manager "${name}" does not exist`);
+    }
+}
+
+/**
+ * Registry singleton managing available {@link TransactionManager}s.
+ */
+@injectable()
+export class TransactionManagerRegistry extends CoreObject {
+    protected registry: Map<string, TransactionManager<any>>;
+    protected defaultManager: string;
+
+    constructor(@inject('SystemConfiguration') sc: SystemConfiguration) {
+        super();
+
+        this.registry = new Map<string, TransactionManager<any>>();
+        this.defaultManager = sc.defaultTransactionManager();
+    }
+
+    /**
+     * Registers the given {@link TransactionManager} under its key.
+     *
+     * @throws {@link DuplicateTransactionManagerError}
+     * Thrown in case of duplicate manager keys
+     *
+     * @param manager Manager to register
+     */
+    public register(manager: TransactionManager<any>): void {
+        const key = manager.type;
+
+        if (this.registry.has(key)) {
+            throw new DuplicateTransactionManagerError(key);
+        }
+
+        this.registry.set(key, manager);
+    }
+
+    /**
+     * Returns the defaut {@link TransactionManager}
+     *
+     * @throws {@link DefaultTransactionManagerDoesNotExistError}
+     * Thrown if the configured default does not exist
+     *
+     * @return Default {@link TransactionManager}
+     */
+    public get default(): TransactionManager<any> {
+        const tm = this.registry.get(this.defaultManager);
+
+        if (!tm) {
+            throw new DefaultTransactionManagerDoesNotExistError(this.defaultManager);
+        }
+
+        return tm;
+    }
+
+    /**
+     * Returns the {@link TransactionManager} for the given name
+     *
+     * @param name Name of the {@link TransactionManager}
+     * @return The {@link TransactionManager}
+     */
+    public forName(name: string): TransactionManager<any> {
+        const tm = this.registry.get(name);
+
+        if (!tm) {
+            throw new TransactionManagerDoesNotExistError(name);
+        }
+
+        return tm;
+    }
+
+    /**
+     * The singleton
+     */
+    protected static instance: TransactionManagerRegistry | null = null;
+
+    /**
+     * Returns the singleton instance of {@link TransactionManagerRegistry}
+     *
+     * @return The instance
+     */
+    public static getInstance(): TransactionManagerRegistry {
+        if (TransactionManagerRegistry.instance === null) {
+            TransactionManagerRegistry.instance = ApplicationContext.getRootInstance().resolve(
+                TransactionManagerRegistry
+            ) as TransactionManagerRegistry;
+        }
+
+        return TransactionManagerRegistry.instance;
+    }
+}
---
+++ core/test/io/datastores/TransactionManagerRegistry.ts
@@ -0,0 +1,188 @@
+import SystemConfiguration from '@core/configuration/SystemConfiguration';
+import { TransactionManager } from '@core/io/datastores/TransactionManager';
+import {
+    DefaultTransactionManagerDoesNotExistError,
+    DuplicateTransactionManagerError,
+    TransactionManagerDoesNotExistError,
+    TransactionManagerRegistry
+} from '@core/io/datastores/TransactionManagerRegistry';
+import ApplicationContext from '@core/ioc/ApplicationContext';
+import test from 'ava';
+import sinon from 'sinon';
+
+const mockConfig = {
+    defaultTransactionManager() {
+        return 'banana';
+    }
+} as SystemConfiguration;
+
+class BaseMockTMR extends TransactionManagerRegistry {
+    constructor() {
+        super(mockConfig);
+    }
+}
+
+/// TransactionManagerRegistry.getInstance
+test('TransactionManagerRegistry.getInstance creates a new instance using the ApplicationContext', t => {
+    t.plan(4);
+
+    class MockTMR extends TransactionManagerRegistry {
+        static get _i(): any {
+            return TransactionManagerRegistry.instance;
+        }
+    }
+
+    const sandbox = sinon.createSandbox();
+    t.teardown(sandbox.restore.bind(sandbox));
+
+    sandbox.stub(ApplicationContext, 'getRootInstance').returns({
+        resolve(klass: any): any {
+            t.is(klass, TransactionManagerRegistry);
+            return 42;
+        }
+    } as ApplicationContext);
+
+    t.is(MockTMR._i, null);
+
+    const instance = TransactionManagerRegistry.getInstance();
+
+    t.is(MockTMR._i, 42);
+    t.is(instance, (42 as unknown) as TransactionManagerRegistry);
+});
+
+test('TransactionManagerRegistry.getInstance returns a singleton', t => {
+    t.plan(1);
+
+    class MockTMR extends TransactionManagerRegistry {
+        static __prepare(): void {
+            TransactionManagerRegistry.instance = (42 as unknown) as TransactionManagerRegistry;
+        }
+    }
+
+    const sandbox = sinon.createSandbox();
+    t.teardown(sandbox.restore.bind(sandbox));
+
+    sandbox.stub(ApplicationContext, 'getRootInstance').set(() => t.fail());
+
+    MockTMR.__prepare();
+
+    const instance = TransactionManagerRegistry.getInstance();
+
+    t.is(instance, (42 as unknown) as TransactionManagerRegistry);
+});
+
+/// TransactionManagerRegistry.constructor
+test('TransactionManagerRegistry.constructor keeps the default transaction manager', t => {
+    t.plan(1);
+
+    class MockTMR extends TransactionManagerRegistry {
+        constructor() {
+            super(mockConfig);
+            t.is(this.defaultManager, 'banana');
+        }
+    }
+
+    new MockTMR();
+});
+
+/// TransactionManagerRegistry.register
+test('TransactionManagerRegistry.register adds the given manager to its registry', t => {
+    t.plan(2);
+
+    const stub = {
+        type: 'banana'
+    } as TransactionManager<any>;
+
+    class MockTMR extends BaseMockTMR {
+        __prepare(): void {
+            t.false(this.registry.has('banana'));
+        }
+
+        __finalize(): void {
+            t.is(this.registry.get('banana'), stub);
+        }
+    }
+
+    const mock = new MockTMR();
+    mock.__prepare();
+    mock.register(stub);
+    mock.__finalize();
+});
+
+test('TransactionManagerRegistry.register throws if manager with that name is already registered', t => {
+    t.plan(1);
+
+    const stub = {
+        type: 'banana'
+    } as TransactionManager<any>;
+
+    class MockTMR extends BaseMockTMR {
+        constructor() {
+            super();
+
+            this.registry.set('banana', stub);
+        }
+    }
+
+    const mock = new MockTMR();
+    t.throws(
+        function () {
+            mock.register(stub);
+        },
+        { instanceOf: DuplicateTransactionManagerError }
+    );
+});
+
+/// TransactionManagerRegistry.default
+test('TransactionManagerRegistry.default returns the default transaction manager', t => {
+    t.plan(1);
+
+    const stub = {
+        type: 'banana'
+    } as TransactionManager<any>;
+
+    const mock = new BaseMockTMR();
+    mock.register(stub);
+
+    t.is(mock.default, stub);
+});
+
+test('TransactionManagerRegistry.default throws if default transaction manager is not registered', t => {
+    t.plan(1);
+
+    const mock = new BaseMockTMR();
+
+    t.throws(
+        function () {
+            mock.default;
+        },
+        { instanceOf: DefaultTransactionManagerDoesNotExistError }
+    );
+});
+
+/// TransactionManagerRegistry.forName
+test('TransactionManagerRegistry.forName returns the transaction manager with the given name', t => {
+    t.plan(1);
+
+    const stub = {
+        type: 'banana'
+    } as TransactionManager<any>;
+
+    const mock = new BaseMockTMR();
+    mock.register(stub);
+
+    t.is(mock.forName('banana'), stub);
+});
+
+test('TransactionManagerRegistry.forName throws if transaction manager does not exist', t => {
+    t.plan(1);
+
+    const mock = new BaseMockTMR();
+
+    t.throws(
+        function () {
+            mock.forName('apple');
+        },
+        { instanceOf: TransactionManagerDoesNotExistError }
+    );
+});
