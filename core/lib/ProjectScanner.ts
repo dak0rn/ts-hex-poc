6f078984919da7ad9f1e7d9d4a2a10736acc1581
Daniel Koch <k@kdb.gmbh>
2020-12-26 13:49:29 +0100

    Add a project scanner

---
+++ core/lib/ProjectScanner.ts
@@ -0,0 +1,59 @@
+import SystemConfiguration from '@core/configuration/SystemConfiguration';
+import CoreObject from '@core/shared/CoreObject';
+import glob from 'glob';
+
+/**
+ * Scanner that loads project files in order to enable usage of decorates
+ * throughout the project. Will scan those folders that are listed in the
+ * `system.scan` configuration array. Scanning is recursive.
+ *
+ * It is important to not implement files with side-effects upon importing.
+ */
+export class ProjectScanner extends CoreObject {
+    protected conf: SystemConfiguration;
+
+    /**
+     * The load/require function for files.
+     * Defaults to `global.require`
+     */
+    protected loader: (path: string) => any;
+
+    /**
+     * Globbing function to get a list of files
+     * Defeaults to `glob.sync`
+     */
+    protected glob: (pattern: string, options: object) => string[];
+
+    /**
+     * Creates a new {@link ProjectScanner}
+     *
+     * @param conf SystemConfiguration
+     */
+    constructor(conf: SystemConfiguration) {
+        super();
+
+        this.conf = conf;
+        this.loader = require;
+        this.glob = glob.sync;
+    }
+
+    /**
+     * Scans the folders configured in `system.scan` for TypeScript files and
+     * requires them
+     */
+    scanAndLoad(): void {
+        const folders = this.conf.projectFolders();
+
+        // Glob through each of the folders
+        let filesToload: string[] = [];
+
+        for (const folder of folders) {
+            filesToload = filesToload.concat(this.glob(`**/*.ts`, { cwd: folder }));
+        }
+
+        // Require each file
+        for (const file of filesToload) {
+            this.loader(file);
+        }
+    }
+}
---
+++ core/test/lib/ProjectScannerTest.ts
@@ -0,0 +1,104 @@
+import SystemConfiguration from '@core/configuration/SystemConfiguration';
+import { ProjectScanner } from '@core/lib/ProjectScanner';
+import test from 'ava';
+import glob from 'glob';
+import sinon from 'sinon';
+
+test('ProjectScanner.constructor sets correct defaults', t => {
+    t.plan(2);
+
+    const sc = {} as SystemConfiguration;
+
+    class MockProjectScanner extends ProjectScanner {
+        constructor() {
+            super(sc);
+
+            t.is(this.conf, sc);
+            // Does not work:
+            // t.is(this.loader, require);
+            t.is(this.glob, glob.sync);
+        }
+    }
+
+    new MockProjectScanner();
+});
+
+test('ProjectScanner.scanAndLoad retrieves a list of files and loads them', t => {
+    t.plan(17);
+
+    const sc = {
+        projectFolders(): string[] {
+            return ['/tmp/a', '/tmp/h', '/tmp/abc/def'];
+        }
+    } as SystemConfiguration;
+
+    function testbed(step: number): { dir: string; files: string[] } {
+        let dir: string;
+        let files: string[];
+
+        switch (step) {
+            case 0:
+                dir = '/tmp/a';
+                files = ['file.ts', 'file2.ts', 'file3.ts'];
+                break;
+            case 1:
+                dir = '/tmp/h';
+                files = ['ello.ts', 'ola.ts', 'allo.ts'];
+                break;
+            case 2:
+                dir = '/tmp/abc/def';
+                files = ['one coconut', 'two cononuts', 'three cononuts'];
+                break;
+            default:
+                throw new Error('Requested testbed too often');
+        }
+
+        return { files, dir };
+    }
+
+    let loadCategory = 0;
+    let loadFile = 0;
+    function load(path: string): any {
+        // 1 test per file name + 1 per group
+
+        // We have three cases per folder
+        // If we'd ran into the fourth, the path should have "changed"
+        if (loadFile > 2) {
+            const prevTestbed = testbed(loadCategory);
+
+            t.false(path.startsWith(prevTestbed.dir));
+
+            loadFile = 0;
+            loadCategory++;
+        }
+
+        const { files, dir } = testbed(loadCategory);
+        const expected = files[loadFile++];
+
+        t.is(path, `${dir}/${expected}`);
+    }
+
+    let globIdx = 0;
+    function glob(path: string, options: any): string[] {
+        // 2 tests per group
+
+        const { files, dir } = testbed(globIdx++);
+
+        t.is(options.cwd, dir);
+        t.is(path, `**/*.ts`);
+
+        return files.map(f => `${dir}/${f}`);
+    }
+
+    class MockProjectScanner extends ProjectScanner {
+        constructor() {
+            super(sc);
+
+            this.glob = glob;
+            this.loader = load;
+        }
+    }
+
+    const mock = new MockProjectScanner();
+    mock.scanAndLoad();
+});
